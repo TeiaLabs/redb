74ecd3a21be760d1007ff713feb00f3708555269
João Severo <33180595+jojo0severo@users.noreply.github.com>
2022-11-18 17:41:20 -0300

    Add insert and find by id to json docs (#3)
    Co-authored-by: jwehrmann <jonataswehrmann@gmail.com>

--- src/redb/json_system/document.py
+++ src/redb/json_system/document.py
@@ -1,5 +1,6 @@
 import json
 from typing import TypeVar, Type
+import pandas as pd
 
 from ..document import Document
 from .. import init_db
@@ -44,3 +45,31 @@ class JSONDocument(Document):
             data = obj
         with open(json_path, "w") as f:
             json.dump(data, f, indent=4)
+
+    @staticmethod
+    def insert_many(cls, data: list[Document] | list[dict]):
+        for item in data:
+            if isinstance(item, dict):
+                item = cls(**item)
+
+            if isinstance(item, cls):
+                item.insert()
+            else:
+                raise ValueError(f"I don't know how to convert item {item} into {cls}")
+
+    @staticmethod
+    def insert_vectors(cls, data: pd.DataFrame):
+        data = [cls(**x) for x in data.to_dict(orient="records")]
+        return cls.insert_many(data)
+
+    @staticmethod
+    def find_by_id(cls, id):
+        c = init_db.REDB.get_client()
+        collection_path = c.attrs["dir_path"] / cls.collection_name()
+        json_path = collection_path / Path(f"{id}.json")
+        if json_path.is_file():
+            with open(json_path) as f:
+                data = json.load(f)
+            return cls(**data)
+        else:
+            return None
