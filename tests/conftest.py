aab5f473c0bc16ec7a587d72a740ac5132247c2f
João Severo <joaovbsevero@gmail.com>
2022-11-22 12:41:07 -0300

    Implement new tests for json system

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+
+from src.redb import RedB
+
+
+@pytest.fixture(scope="session")
+def client_path():
+    return Path(".")
+
+
+@pytest.fixture(scope="session")
+def db_path(client_path):
+    return client_path / "resources"
+
+
+@pytest.fixture(scope="session")
+def collection_path(db_path):
+    return db_path / "Embedding"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def client(client_path, db_path):
+    RedB.setup("json", client_path=client_path, database_path=db_path)
--- tests/test_json_client.py
+++ tests/test_json_client.py
@@ -1,44 +1,77 @@
-import pytest
+import json
 from pathlib import Path
 
-from redb.init_db import REDB
-from redb.document import Document
+import pytest
+
+from .utils import Embedding, assert_exists, compare_values, remove_document
 
 
-class Embedding(Document):
-    kb_name: str
-    # metadata: dict[str, str]
-    model: str
-    text: str
-    vector: list[float] = None
-    source_url: str
+def test_insert_one(collection_path: Path):
+    d = Embedding(
+        kb_name="KB",
+        model="ai",
+        text="Some data.",
+        vector=[1, 2],
+        source_url="www",
+    )
+    d.insert_one()
+    assert_exists(collection_path, d.get_hash())
 
-    def get_hash(self):
-        return super().get_hash(
-            fields=[
-                'kb_name',
-                'model',
-                'text',
-                'source_url',
-            ],
+    json_path = collection_path / f"{d.get_hash()}.json"
+    other = json.load(open(json_path))
+
+    remove_document(collection_path, d.get_hash())
+    compare_values(other, d)
+
+
+def test_find_by_id(collection_path: Path):
+    d = Embedding(
+        kb_name="KB",
+        model="ai",
+        text="Some data.",
+        source_url="www",
+    )
+    d.insert_one()
+    assert_exists(collection_path, d.get_hash())
+
+    found_instance = Embedding.find_by_id(d.get_hash())
+
+    remove_document(collection_path, d.get_hash())
+    compare_values(found_instance, d)
+
+
+def test_insert_vectors(collection_path: Path):
+    data = dict(
+        kb_name=["KB5", "KB6", "KB7"],
+        model=["ai", "ai", "ai"],
+        text=["Some data 5", "Some data 6", "Some data 7"],
+        vector=[[1, 3], [2, 4], [3, 5]],
+        source_url=["ww5", "ww6", "ww7"],
+    )
+    Embedding.insert_vectors(data)
+
+    embeddings = [
+        Embedding(**json.load(open(val))) for val in collection_path.glob("*.json")
+    ]
+    assert len(embeddings) == 3
+
+    embeddings.sort(key=lambda embedding: embedding.kb_name)
+
+    for i in range(len(embeddings)):
+        original = Embedding(
+            kb_name=data["kb_name"][i],
+            model=data["model"][i],
+            text=data["text"][i],
+            vector=data["vector"][i],
+            source_url=data["source_url"][i],
         )
-
-    @classmethod
-    def find_by_id(cls, id):
-        return super().find_by_id(id)
-
-
-@pytest.fixture
-def init_test():
-    import shutil
-    db_dir = Path(".temp/")
-    # remove .temp/ with all elements
-    shutil.rmtree(db_dir, ignore_errors=True)
-    db_dir.mkdir(exist_ok=True)
-    return REDB("json", dict(dir_path=db_dir))
+        other = embeddings[i]
+        remove_document(collection_path, other.get_hash())
+        compare_values(original, other)
 
 
-def test_insert(init_test):
+def test_count_documents(collection_path: Path):
+    assert Embedding.count_documents() == 0
 
     d = Embedding(
         kb_name="KB",
@@ -47,45 +80,76 @@ def test_insert(init_test):
         vector=[1, 2],
         source_url="www",
     )
-    d.insert()
+    d.insert_one()
+    assert_exists(collection_path, d.get_hash())
 
-    current_hash = d.get_hash()
+    assert Embedding.count_documents() == 1
 
-    found_instance = (Embedding.find_by_id(current_hash))
-    assert found_instance.get_hash() == current_hash
+    remove_document(collection_path, d.get_hash())
 
-    # Hash should not take into account the vector
+
+def test_replace_one(collection_path: Path):
     d = Embedding(
         kb_name="KB",
         model="ai",
         text="Some data.",
+        vector=[1, 2],
         source_url="www",
     )
+    d.insert_one()
+    assert_exists(collection_path, d.get_hash())
 
-    found_instance = Embedding.find_by_id(d.get_hash())
-    assert found_instance.get_hash() == d.get_hash()
-
-
-def test_insert_many(init_test):
-    import pandas as pd
-
-    df_columns = list(Embedding.__fields__.keys())
-    df = pd.DataFrame(
-        columns=df_columns,
-        data=[
-            ['KB5', 'ai', 'Some data 5', [1, 3], 'ww5'],
-            ['KB6', 'ai', 'Some data 6', [2, 4], 'ww6'],
-            ['KB7', 'ai', 'Some data 7', [3, 5], 'ww7'],
-        ],
+    replacement = Embedding(
+        kb_name="KB_REPLACED",
+        model="replaced",
+        text="Some replaced data.",
+        vector=[114, 101, 112, 108, 97, 99, 101, 100],
+        source_url="www.replaced",
     )
-    Embedding.insert_vectors(df)
 
-    df.text = ['a', 'b', 'c']
-    dicts = df.to_dict(orient='records')
-    Embedding.insert_many(dicts)
+    d.replace_one(replacement)
 
-    data = Embedding.find_many()
-    assert len(data) == 6
-    dfa = pd.DataFrame(data=[x.dict() for x in data])
-    assert dfa.shape == (6, 5)
-    assert set(dfa.columns) == set(df_columns)
+    other = json.load(open(collection_path / f"{d.get_hash()}.json"))
+
+    remove_document(collection_path, d.get_hash())
+    compare_values(replacement, other)
+
+
+def test_update_one(collection_path: Path):
+    d = Embedding(
+        kb_name="KB",
+        model="ai",
+        text="Some data.",
+        vector=[1, 2],
+        source_url="www",
+    )
+    d.insert_one()
+    assert_exists(collection_path, d.get_hash())
+
+    update = Embedding.construct(kb_name="KB_UPDATED")
+    d.update_one(update)
+
+    expected = d.dict()
+    expected["kb_name"] = "KB_UPDATED"
+    other = json.load(open(collection_path / f"{d.get_hash()}.json"))
+
+    remove_document(collection_path, d.get_hash())
+    print(expected, other)
+    compare_values(expected, other)
+
+
+def test_delete_one(collection_path: Path):
+    d = Embedding(
+        kb_name="KB",
+        model="ai",
+        text="Some data.",
+        vector=[1, 2],
+        source_url="www",
+    )
+    d.insert_one()
+    assert_exists(collection_path, d.get_hash())
+
+    d.delete_one()
+
+    with pytest.raises(AssertionError):
+        assert_exists(collection_path, d.get_hash())
