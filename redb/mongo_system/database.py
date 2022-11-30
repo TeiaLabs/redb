f06fce34ddcc2d72d85f24caa5429f684553f6db
João Severo <joaovbsevero@gmail.com>
2022-11-30 14:58:12 -0300

    Add underscore for private member

--- redb/mongo_system/database.py
+++ redb/mongo_system/database.py
@@ -6,22 +6,22 @@ from .collection import MongoCollection
 
 class MongoDatabase(Database):
     def __init__(self, database: PymongoDatabase) -> None:
-        self.database = database
+        self.__database = database
 
     def _get_driver_database(self) -> "Database":
-        return self.database
+        return self.__database
 
     def get_collections(self) -> list[MongoCollection]:
-        return list(self.database.list_collections())
+        return list(self.__database.list_collections())
 
     def get_collection(self, name: str) -> MongoCollection:
         return MongoCollection.__new__(MongoCollection, name)
 
     def create_collection(self, name: str) -> None:
-        self.database.create_collection(name)
+        self.__database.create_collection(name)
 
     def delete_collection(self, name: str) -> None:
-        self.database.drop_collection(name)
+        self.__database.drop_collection(name)
 
     def __getitem__(self, name) -> MongoCollection:
         return self.get_collection(name)
