46fa0845b784eb7139473282e2d1c7e2b3a1d2d0
João Severo <joaovbsevero@gmail.com>
2022-12-20 02:02:24 -0300

    Add mongo usage example

--- resources/examples/mongo_example.py
+++ resources/examples/mongo_example.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import os
-import dotenv
 from datetime import datetime
 
-from redb import Document, RedB
+import dotenv
+
+from redb import Document, Field, FieldIndice, RedB
 from redb.mongo_system import MongoConfig
 
 dotenv.load_dotenv()
@@ -16,16 +17,33 @@ class Dog(Document):
     # friends: list[Dog] = []  # TODO: make this work
 
 
+class Embedding(Document):
+    kb_name: str
+    model: str = Field(index=FieldIndice(group_name="nop"))
+    text: str = Field(index=FieldIndice(group_name="hu3"))
+    vector: list[float]
+    source_url: str = Field(index=FieldIndice(group_name="hu3", name="sourceUrl"))
+
+
 def main():
     config = MongoConfig(database_uri=os.environ["MONGODB_URI"])
     RedB.setup(config=config)
     client = RedB.get_client("mongo")
     db = client.get_default_database()
     print([col for col in db.get_collections()])
+    
     d = Dog(
         name="Spike",
         birthday=datetime.today(),
-        friends=[Dog(name="Lily", birthday=datetime.today())]
+        friends=[Dog(name="Lily", birthday=datetime.today())],
+    )
+
+    d = Embedding(
+        kb_name="KB",
+        model="big-and-strong",
+        text="Some data.",
+        vector=[1, 2, 0.1],
+        source_url="www",
     )
     print(d)
     print(Dog.delete_many(d))
