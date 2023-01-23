30e9ce3fa9839f94ce6681bdff786f943c0da28f
Jônatas Wehrmann <jonataswehrmann@gmail.com>
2023-01-23 10:12:43 -0300

    My enormous contribution

--- src/redb/__init__.py
+++ src/redb/__init__.py
@@ -1,3 +1,4 @@
 from .document import Document
 from .instance import RedB
 from redb.interface.fields import CompoundIndex, Field, Index, ClassField
+from redb.interface.configs import JSONConfig, MongoConfig, MigoConfig
