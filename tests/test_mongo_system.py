01fdbe40b991ef58a9976161fd26e9d5b972fc26
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-02-09 18:00:15 -0300

    Add test to mongo update_one

--- tests/test_mongo_system.py
+++ tests/test_mongo_system.py
@@ -6,6 +6,7 @@ from redb.core import RedB
 from redb.interface.configs import MongoConfig
 from .utils import Embedding, RussianDog
 
+
 class TestmongoSystem:
     @pytest.fixture()
     def client_path(self):
@@ -32,7 +33,7 @@ class TestmongoSystem:
             source_url="www",
         )
         emb_id = Embedding.insert_one(emb)
-        response = Embedding.find_one({"_id":emb_id.inserted_id})
+        response = Embedding.find_one({"_id": emb_id.inserted_id})
         assert emb == response
 
     def test_find_one(self):
@@ -44,13 +45,51 @@ class TestmongoSystem:
             is_good_boy=True,
         )
         vladmir_id = RussianDog.insert_one(vladmir)
-        vladmir_from_db = RussianDog.find_one({"_id":vladmir_id.inserted_id})
+        vladmir_from_db = RussianDog.find_one({"_id": vladmir_id.inserted_id})
         vladmir_breed = RussianDog.find_one(vladmir, fields=["breed"])
-        assert vladmir_breed == {"_id":vladmir_id.inserted_id,"breed": "Siberian Husky"}
+        assert vladmir_breed == {
+            "_id": vladmir_id.inserted_id,
+            "breed": "Siberian Husky",
+        }
         assert vladmir == vladmir_from_db
 
     def test_update_one(self):
+        boris = RussianDog(
+            name="Boris",
+            age=2,
+            breed="Siberian Husky",
+            color="Gray",
+            is_good_boy=True,
+        )
 
-        pass
+        boris_id = RussianDog.insert_one(boris)
+        RussianDog.update_one(filter=boris, update={"age": 3})
+        boris.age = 3
+        upsert_result = RussianDog.update_one(
+            filter=boris, update={"is_good_boy": False}, upsert=True
+        )
+        boris_from_db = RussianDog.find_one({"_id": boris_id.inserted_id})
+
+        assert upsert_result.matched_count == 1
+        assert upsert_result.modified_count == 1
+        assert boris_from_db.age == 3
+        assert boris_from_db.is_good_boy == False
+
+        irina = RussianDog(
+            name="Irina",
+            age=2,
+            breed="Siberian Husky",
+            color="Gray",
+            is_good_boy=True,
+        )
+        upsert_result = RussianDog.update_one(
+            filter=irina, update={"color": "White"}, upsert=True
+        )
+        irina.color = "White"
+        irina_from_db = RussianDog.find_one({"_id": upsert_result.upserted_id})
+        assert irina == irina_from_db
+        assert upsert_result.matched_count == 0
+        assert upsert_result.modified_count == 0
+
     def test_delete_one(self):
         pass
