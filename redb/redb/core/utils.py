57ccf354f3e3cadece7fe1c2ab1408bcb529e21c
João Severo <33180595+jojo0severo@users.noreply.github.com>
2023-05-03 10:53:56 -0300

    Fix schezophrenia of type_ and outer_type_ being the same thing (#89)

--- redb/redb/core/utils.py
+++ redb/redb/core/utils.py
@@ -1,7 +1,9 @@
+import types
 from enum import Enum
 from typing import Any, Type, TypeVar
 
 from pydantic import BaseModel
+from pydantic.fields import ModelField
 
 T = TypeVar("T", bound=BaseModel)
 
@@ -63,21 +65,48 @@ def _get_value(pydantic_model: Type[T]) -> dict:
             value[name] = field_value
             continue
 
-        if issubclass(field.type_, BaseModel):
-            value[name] = _get_value(field.type_)
-        elif issubclass(field.type_, Enum):
-            value[name] = next(iter(field.type_)).value
-        elif field.type_.__name__ in DEFAULT_VALUES:
-            value[name] = DEFAULT_VALUES[field.type_.__name__]
-        else:
-            value[name] = field.type_.__name__
+        value[name] = _parse_value(field)
 
-        if name in value and field.outer_type_.__name__ in DEFAULT_CONTAINER_VALUES:
-            if field.outer_type_.__name__ == "list":
-                value[name] = [value[name]]
-            elif field.outer_type_.__name__ == "set":
-                value[name] = {value[name]}
-            elif field.outer_type_.__name__ == "tuple":
-                value[name] = (value[name],)
+    return value
 
+
+def _parse_value(field: ModelField):
+    value = None
+    if isinstance(field.type_, types.UnionType):
+        field.type_ = field.type_.__args__[0]
+
+    if isinstance(field.outer_type_, types.UnionType):
+        field.outer_type_ = field.outer_type_.__args__[0]
+
+    if field.type_.__name__ in DEFAULT_CONTAINER_VALUES:
+        try:
+            tmp = field.type_
+            field.type_ = field.type_.__args__[0]
+            field.outer_type_ = tmp
+        except:
+            return DEFAULT_CONTAINER_VALUES[field.type_.__name__]
+
+    if field.type_.__name__ in DEFAULT_PRIMITIVE_VALUES:
+        value = DEFAULT_PRIMITIVE_VALUES[field.type_.__name__]
+    elif issubclass(field.type_, BaseModel):
+        value = _get_value(field.type_)
+    elif issubclass(field.type_, Enum):
+        value = next(iter(field.type_)).value
+    else:
+        value = field.type_.__name__
+
+    if field.outer_type_.__name__ in DEFAULT_CONTAINER_VALUES:
+        value = _build_container(value, field)
+    
+    return value
+        
+
+
+def _build_container(value: Any, field: ModelField):
+    if field.outer_type_.__name__ == "list":
+        return [value] if value is not None else []
+    elif field.outer_type_.__name__ == "set":
+        return {value,} if value is not None else set()
+    elif field.outer_type_.__name__ == "tuple":
+        return (value,) if value is not None else tuple()
     return value
