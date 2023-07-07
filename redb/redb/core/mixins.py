be18d264197fc444d2a0ffdf89e009e067f8adce
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-07-07 20:00:01 -0300

    Add DB switching mixin

---
+++ redb/redb/core/mixins.py
@@ -0,0 +1,13 @@
+from typing import Type, TypeVar
+
+from redb.core import transaction
+
+T = TypeVar("T")
+
+
+class SwitcharooMixin:
+
+    @classmethod
+    def switch_db(cls: Type[T], db_name: str) -> Type[T]:
+        with transaction.transaction(cls, db_name) as new_cls:
+            return new_cls
