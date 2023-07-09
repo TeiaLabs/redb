2d012a81b815489864a30027c2f9885dcab4fbdc
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-07-09 01:16:42 -0300

    Fix positional vs keyword confusion

--- redb/redb/core/mixins.py
+++ redb/redb/core/mixins.py
@@ -9,5 +9,5 @@ class SwitcharooMixin:
 
     @classmethod
     def switch_db(cls: Type[T], db_name: str) -> Type[T]:
-        with transaction.transaction(cls, db_name) as new_cls:
+        with transaction.transaction(cls, db_name=db_name) as new_cls:
             return new_cls
