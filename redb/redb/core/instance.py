e20f66600bc5ff6d6fcf53a79f22f36313f17c7a
João Severo <33180595+jojo0severo@users.noreply.github.com>
2023-07-14 17:13:29 -0300

    Properly support new client connections (#96)

--- redb/redb/core/instance.py
+++ redb/redb/core/instance.py
@@ -12,27 +12,96 @@ from redb.interface.configs import (
 class RedB:
     """Client singleton."""
 
-    _client = None
+    _clients = None
+    _alias: dict[str, int] = {}
+    _uris: dict[str, int] = {}
     _client_name: str | None = None
-    _config: CONFIG_TYPE | None = None
+    _configs: list[CONFIG_TYPE] | None = None
 
     @classmethod
-    def get_client(cls):
-        if cls._client is None:
+    def add_client(
+        cls, config: MongoConfig | dict | None = None, alias: str | None = None
+    ):
+        if cls._clients is None:
             raise RuntimeError("Client not setup. Call setup() first.")
-        return cls._client
+
+        from redb.mongo_system import MongoClient
+
+        if config is None:
+            if alias is None:
+                msg = "No config or alias informed"
+                raise ValueError(msg)
+
+            if alias not in cls._alias:
+                msg = f"Alias '{alias}' not found"
+                raise ValueError(msg)
+
+            alias_index = cls._alias[alias]
+            return cls._clients[alias_index]
+
+        if isinstance(config, dict):
+            database_uri = config["database_uri"]
+        else:
+            database_uri = config.database_uri
+
+        alias_index = None
+        if alias is not None and alias in cls._alias:
+            alias_index = cls._alias[alias]
+
+        uri_index = None
+        if database_uri in cls._uris:
+            uri_index = cls._uris[database_uri]
+
+        if (
+            alias_index is not None
+            and uri_index is not None
+            and alias_index != uri_index
+        ):
+            msg = f"Alias '{alias}' does not match already configured connection '{database_uri}'"
+            raise ValueError(msg)
+
+        if alias_index is not None:
+            return cls._clients[alias_index]
+
+        if uri_index is not None:
+            return cls._clients[uri_index]
+
+        client = MongoClient(config)
+        client_index = len(cls._clients)
+
+        cls._clients.append(client)  # type: ignore
+        cls._uris[database_uri] = client_index
+        if alias is not None:
+            cls._alias[alias] = client_index
+
+        return client
+
+    @classmethod
+    def get_client(cls, index: int = 0, uri: str = ""):
+        if cls._clients is None:
+            raise RuntimeError("Client not setup. Call setup() first.")
+
+        if uri:
+            if cls._uris is None:
+                raise ValueError(f"Searching for URI '{uri}' with no URI configured.")
+            elif uri not in cls._uris:
+                raise ValueError(f"URI '{uri}' not found.")
+
+            index = cls._uris[uri]
+
+        return cls._clients[index]
 
     @classmethod
     def get_client_name(cls) -> str:
         if cls._client_name is None:
             raise RuntimeError("Client not setup. Call setup() first.")
         return cls._client_name
-    
+
     @classmethod
     def get_config(cls) -> CONFIG_TYPE:
-        if cls._config is None:
+        if cls._configs is None:
             raise RuntimeError("Client not setup. Call setup() first.")
-        return cls._config
+        return cls._configs[0]
 
     @classmethod
     def setup(
@@ -47,7 +116,7 @@ class RedB:
         ):
             from redb.json_system import JSONClient
 
-            cls._client = JSONClient(config)
+            cls._clients = [JSONClient(config)]
             cls._client_name = "json"
 
         elif backend == "mongo" or (
@@ -55,7 +124,8 @@ class RedB:
         ):
             from redb.mongo_system import MongoClient
 
-            cls._client = MongoClient(config)
+            cls._clients = [MongoClient(config)]
+            cls._uris = {config.database_uri: 0}
             cls._client_name = "mongo"
 
         elif backend == "migo" or (
@@ -63,10 +133,10 @@ class RedB:
         ):
             from redb.migo_system import MigoClient
 
-            cls._client = MigoClient(config)
+            cls._clients = [MigoClient(config)]
             cls._client_name = "migo"
 
         else:
             raise ValueError(f"Backend not found for config type: {type(config)!r}.")
-        
-        cls._config = config
+
+        cls._configs = [config]
